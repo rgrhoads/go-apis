764d84bb3df8e0fe5e5ae0f091e0957373036173
ryanrhoads <rrhoads@graymatters-inc.com>
2023-11-28 11:18:47 -0500

    Add test dat struct and test data end point

--- main/main.go
+++ main/main.go
@@ -2,20 +2,32 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 // import "fmt"
+type TestData struct {
+	Data    string `json:"data"`
+	Message string `json:"message"`
+}
 
 func main() {
 	router := gin.Default()
 
 	router.GET("/", welcome)
+	router.GET("/test-data", testData)
 
 	router.Run(":3773")
 }
 
 func welcome(c *gin.Context) {
 	log.Printf("Welcome to Modren APIs!")
+}
+
+func testData(c *gin.Context) {
+	response := TestData{"12345", "Hello World!"}
+
+	c.JSON(http.StatusOK, response)
 }
